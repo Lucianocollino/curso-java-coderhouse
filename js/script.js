b93ac217c4d58f510bdec334b30d45ad9a242098
Luciano Collino <lucianocollino94@gmail.com>
2024-06-02 23:07:03 -0300

    agregue un array hamburguesas

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@ let nombre = prompt("ingrese su nombre")
 let mesa = prompt("ingrese numero de mesa")
 
 console.log (nombre,mesa)
-alert(" mi nombre es "+nombre+", mesa numero"+mesa )
+alert(" mi nombre es "+nombre+", mesa numero "+mesa )
 
-let continuar = true
+const Hamburguesas = ["hamburguesa clasica", "hamburguesa cheddar", "hamburguesa de campo",]
+let continuar=true
 while(continuar) {
     let Hamburguesas = parseInt(prompt("Ingrese 1 para Hamburguesa clasica, 2 para Hamburguesa cheddar, 3 para Hamburguesa de campo, otro numero para salir"))
     switch(Hamburguesas) {
@@ -37,10 +38,10 @@ while(continuar) {
             break
     }
 
-    let confirmacion = prompt("Desea hacer otra consulta? (si/no)")
+    let confirmacion = prompt("Desea agregar algo a su pedido? (si/no)")
     if(confirmacion == "no"){
         continuar=false
-        console.log("Gracias!")
+       
     }
 }
 
@@ -48,7 +49,15 @@ function sumar(Hamburguesa,bebida) {
    
     let resultado = Hamburguesa + bebida
     console.log("Total de su compra:"+ resultado)
+    alert("Total de su compra:"+ resultado +"gracias por su compra")
     
       }
 
       sumar(5500,1500)
+
+       
+
+       const Hamburguesa = ["lechuga", "carne", "tomate", "pan", "queso"]
+       
+
+       
